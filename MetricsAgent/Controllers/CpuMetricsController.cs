9aa7a105c16ecdbaa211d240ff2f1f3c295ea84e
Jackson <dmitrievei92@icloud.com>
2021-06-16 13:33:48 +0300

    update cpu_metrics

--- MetricsAgent/Controllers/CpuMetricsController.cs
+++ MetricsAgent/Controllers/CpuMetricsController.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using MetricsAgent.MetricClasses;
+using MetricsAgent.DAL;
+using MetricsAgent.Requests;
+using MetricsAgent.Responses;
 
 namespace MetricsAgent.Controllers
 {
@@ -14,21 +17,67 @@ namespace MetricsAgent.Controllers
     [ApiController]
     public class CpuMetricsController : ControllerBase
     {
-        private readonly ILogger<CpuMetricsController> _logger;
 
-        public CpuMetricsController(ILogger<CpuMetricsController> logger)
+        private ICpuMetricsRepository repository;
+        private readonly ILogger<CpuMetricsController> _logger;
+        public CpuMetricsController(ICpuMetricsRepository repository, ILogger<CpuMetricsController> logger)
         {
+            this.repository = repository;
             _logger = logger;
             _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
         }
-
-
+        
         [HttpGet("from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsByTime([FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime)
         {
             _logger.LogInformation($"Метрика ЦПУ с {fromTime} по {toTime}");
+            fromTime = new DateTimeOffset(fromTime.UtcDateTime);
+            toTime = new DateTimeOffset(toTime.UtcDateTime);
+
+            var metrics = repository.GetByTimePeriod(fromTime, toTime);
+
+            var response = new AllCpuMetricsResponse()
+            {
+                Metrics = new List<CpuMetricDto>()
+            };
+
+            foreach (var metric in metrics)
+            {
+                response.Metrics.Add(new CpuMetricDto { Time = metric.Time, Value = metric.Value, Id = metric.Id });
+            }
 
             return Ok();
         }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] CpuMetricCreateRequest request)
+        {
+            repository.Create(new CpuMetric
+            {
+                Time = request.Time,
+                Value = request.Value
+            });
+
+            return Ok();
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            var metrics = repository.GetAll();
+
+            var response = new AllCpuMetricsResponse()
+            {
+                Metrics = new List<CpuMetricDto>()
+            };
+
+            foreach (var metric in metrics)
+            {
+                response.Metrics.Add(new CpuMetricDto { Time = metric.Time, Value = metric.Value, Id = metric.Id });
+            }
+
+            return Ok(response);
+        }
+
     }
 }
--- MetricsAgent/DAL/CpuMetricsRepository.cs
+++ MetricsAgent/DAL/CpuMetricsRepository.cs
@@ -17,65 +17,16 @@ namespace MetricsAgent.DAL
     public class CpuMetricsRepository : ICpuMetricsRepository
     {
         private const string ConnectionString = "Data Source=metrics.db;Version=3;Pooling=true;Max Pool Size=100";
-        // инжектируем соединение с базой данных в наш репозиторий через конструктор
+        
 
-        public void Create(CpuMetric item)
-        {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            // создаем команду
-            using var cmd = new SQLiteCommand(connection);
-            // прописываем в команду SQL запрос на вставку данных
-            cmd.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(@value, @time)";
-
-            // добавляем параметры в запрос из нашего объекта
-            cmd.Parameters.AddWithValue("@value", item.Value);
-
-            // в таблице будем хранить время в секундах, потому преобразуем перед записью в секунды
-            // через свойство
-            cmd.Parameters.AddWithValue("@time", item.Time.Second);
-            // подготовка команды к выполнению
-            cmd.Prepare();
-
-            // выполнение команды
-            cmd.ExecuteNonQuery();
-        }
-
-        public void Delete(int id)
+        public IList<CpuMetric> GetByTimePeriod(DateTimeOffset fromTime, DateTimeOffset toTime)
         {
             using var connection = new SQLiteConnection(ConnectionString);
             connection.Open();
             using var cmd = new SQLiteCommand(connection);
-            // прописываем в команду SQL запрос на удаление данных
-            cmd.CommandText = "DELETE FROM cpumetrics WHERE id=@id";
-
-            cmd.Parameters.AddWithValue("@id", id);
-            cmd.Prepare();
+            string comText = $"SELECT * FROM metrics WHERE (time > {fromTime.ToUnixTimeSeconds()}) AND (time < {toTime.ToUnixTimeSeconds()})";
+            cmd.CommandText = comText;
             cmd.ExecuteNonQuery();
-        }
-
-        public void Update(CpuMetric item)
-        {
-            using var connection = new SQLiteConnection(ConnectionString);
-            using var cmd = new SQLiteCommand(connection);
-            // прописываем в команду SQL запрос на обновление данных
-            cmd.CommandText = "UPDATE cpumetrics SET value = @value, time = @time WHERE id=@id;";
-            cmd.Parameters.AddWithValue("@id", item.Id);
-            cmd.Parameters.AddWithValue("@value", item.Value);
-            cmd.Parameters.AddWithValue("@time", item.Time.Second);
-            cmd.Prepare();
-
-            cmd.ExecuteNonQuery();
-        }
-
-        public IList<CpuMetric> GetAll()
-        {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-
-            // прописываем в команду SQL запрос на получение всех данных из таблицы
-            cmd.CommandText = "SELECT * FROM cpumetrics";
 
             var returnList = new List<CpuMetric>();
 
@@ -84,47 +35,21 @@ namespace MetricsAgent.DAL
                 // пока есть что читать -- читаем
                 while (reader.Read())
                 {
-                    // добавляем объект в список возврата
+
                     returnList.Add(new CpuMetric
                     {
                         Id = reader.GetInt32(0),
                         Value = reader.GetInt32(1),
                         // налету преобразуем прочитанные секунды в метку времени
                         Time = DateTimeOffset.FromUnixTimeSeconds(reader.GetInt32(2))
-                        
                     });
                 }
             }
 
             return returnList;
         }
-
-        public CpuMetric GetById(int id)
-        {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "SELECT * FROM cpumetrics WHERE id=@id";
-            using (SQLiteDataReader reader = cmd.ExecuteReader())
-            {
-                // если удалось что то прочитать
-                if (reader.Read())
-                {
-                    // возвращаем прочитанное
-                    return new CpuMetric
-                    {
-                        Id = reader.GetInt32(0),
-                        Value = reader.GetInt32(1),
-                        Time = DateTimeOffset.FromUnixTimeSeconds(reader.GetInt32(2))
-                    };
-                }
-                else
-                {
-                    // не нашлось запись по идентификатору, не делаем ничего
-                    return null;
-                }
-            }
-        }
     }
+
+
 
 }
